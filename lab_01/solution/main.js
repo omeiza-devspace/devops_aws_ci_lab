0dd7295f852114547d220b58c5b0fb211a74a270
Okuta Omeiza <okuta.omeiza@gmail.com>
2023-06-21 03:14:39 +0100

    Test question for lab_01 added

--- lab_01/solution/main.js
+++ lab_01/solution/main.js
@@ -10,6 +10,6 @@ console.log('Server has started');
 function onRequest(request, response){
   const host = request.socket.localAddress;
   response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write(`Hi there! I'm being served from ${hostname} on ${host}:${port}/`);
+  response.write(`Hi there! I'm being served from ${hostname}`);
   response.end();
 }
