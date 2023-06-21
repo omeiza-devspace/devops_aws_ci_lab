01327e1acd80395ac4f4861db1e6b05ace0f0c4a
Okuta Omeiza <okuta.omeiza@gmail.com>
2023-06-21 03:37:08 +0100

    updated setu_docker_app script

--- lab_01/solution/setup_docker_app.sh
+++ lab_01/solution/setup_docker_app.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
-git clone https://github.com/Stonnystone/devops_aws_ci_lab.git
-cd devops_aws_ci_lab.git/aws
+curl https://github.com/Stonnystone/devops_aws_ci_lab/blob/main/lab_01/solution/Dockerfile > Dockerfile
+curl https://github.com/Stonnystone/devops_aws_ci_lab/blob/main/lab_01/solution/main.js > main.js
+
+sudo docker build -t demo-app --name demo-app .
+ 
+sudo docker run -it -d -p 80:3000 demo-app
  
