27ed91bb3336d974923adb4aba8225a2c7c41bd2
Okuta Omeiza <okuta.omeiza@gmail.com>
2023-06-21 04:38:45 +0100

    nginx script and config refactoring

--- lab_01/solution/setup_proxy.sh
+++ lab_01/solution/setup_proxy.sh
@@ -16,9 +16,11 @@ sudo yum install epel-release
 sudo yum update  -y
 sudo yum install -y nginx  git
 sudo systemctl enable nginx
-sudo systemctl restart nginx
 nginx -v
 
-curl https://github.com/Stonnystone/devops_aws_ci_lab/blob/main/lab_01/solution/nginx.conf > nginx.conf
-sudo cp nginx.conf /etc/nginx/nginx.conf
+sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf_orig 
+
+curl https://github.com/Stonnystone/devops_aws_ci_lab/blob/main/lab_01/solution/nginx.conf > nginx_config
+sudo cp nginx_config /etc/nginx/nginx.conf
+sudo nginx -t
 sudo systemctl restart nginx
